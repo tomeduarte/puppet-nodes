89e2a54ecffbf53d0b5a23a68ac8b84f51646213
Tomé Duarte <tome.duarte@gmail.com>
2012-03-27 11:41:40 +0100

    MySQL class (joins server and client class).

--- site.pp
+++ site.pp
@@ -17,7 +17,24 @@ class timezone($zone='Etc/UTC') {
   }
 }
 
+# MYSQL
+class mysql_client {
+  package { 'mysql-client':
+    ensure  =>  latest,
+  }
+}
+class mysql_server {
+  package { 'mysql-server':
+    ensure  =>  latest,
+  }
+}
+class mysql {
+  include mysql_client
+  include mysql_server
+}
+
 # NODES
 node default {
   include timezone
+  include mysql
 }
