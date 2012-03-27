b132219dae4c8be09a7c2af10fcdf988c98a4be8
Tomé Duarte <tome.duarte@gmail.com>
2012-03-27 16:20:38 +0100

    Added Java packages (sun / openjdk)

--- site.pp
+++ site.pp
@@ -33,8 +33,37 @@ class mysql {
   include mysql_server
 }
 
+# JAVA
+class sun_jre_6 {
+  package { 'debconf-utils':
+    ensure  => installed
+  }
+
+  exec { "agree-license":
+    command => "/bin/echo -e sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true | debconf-set-selections",
+    unless  => "debconf-get-selections | grep 'sun-java6-jre.*shared/accepted-sun-dlj-v1-1.*true'",
+    path    => ["/usr/bin", "/bin"],
+    require => Package["debconf-utils"],
+  }
+
+  package { 'sun-java6-jre':
+    ensure  =>  latest,
+    require => Exec["agree-license"],
+  }
+}
+
+class java_runtimes {
+  include sun_jre_6
+
+  package { 'OpenJDK JRE':
+    name    =>  'openjdk-6-jre',
+    ensure  =>  latest,
+  }
+}
+
 # NODES
 node default {
   include timezone
   include mysql
+  include java_runtimes
 }
