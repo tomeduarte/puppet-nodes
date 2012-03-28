09613de090704d97e251410ee223e8e19a73665a
Tomé Duarte <tome.duarte@gmail.com>
2012-03-28 03:53:36 +0100

    Split special/regular nodes for backup configuration.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,21 @@
-# NODES
+# file buckets
+  filebucket { 'main':
+    server  => 'puppet',
+    path    => false,
+  }
+
+# nodes
 node default {
   include timezone
   include common_packages
   include agent_service
+
+  # no backups for normal nodes
+  File { backup  => false, }
+}
+
+node /^special\d+$/ inherits default {
+  notify { 'special node, backing up':}
+  # we want backups on special nodes
+  File { backup => 'main', }
 }
